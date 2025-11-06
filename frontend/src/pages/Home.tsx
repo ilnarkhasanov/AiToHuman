76a564969ea7f3e042bcb2f7f612b2f639c8bef9
G-Akleh <ghadeer_akleh@hotmail.com>
2025-11-06 03:47:29 +0300

    Adjust main component style

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,14 +4,14 @@ function Home() {
   return (
     <div id="page-background" className="bg-gray-100">
       <main className="w-full max-w-3xl mx-auto min-h-screen">
-        <h1 className="text-center text-3xl sm:text-4xl text-gray-800 pt-6 pb-2 font-lato font-extrabold">
+        <h1 className="text-center text-3xl sm:text-4xl text-gray-800 pt-6 pb-4 font-lato font-extrabold">
           Detect and Humanize AI Text
         </h1>
-        <p className="text-center text-lg text-gray-600 mb-8 font-lato">
+        <p className="text-center text-xl text-gray-600 mb-8 font-lato">
           Paste your text below to analyze for AI-generated content and
           transform it to a human-like style.
         </p>
-        <div className="bg-white rounded-lg shadow-lg p-5 ">
+        <div className="bg-white rounded-lg shadow-lg p-8 ">
           {/* TODO: status should be dynamic */}
           <TextProcessor status="idle" />
         </div>

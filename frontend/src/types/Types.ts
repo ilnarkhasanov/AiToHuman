e0df80c35adf402ea9990240d03fdcb658b2f087
G-Akleh <ghadeer_akleh@hotmail.com>
2025-11-15 02:31:29 +0300

    Add endpoint calls

--- frontend/src/types/Types.ts
+++ frontend/src/types/Types.ts
@@ -4,3 +4,27 @@ export type AppStatus =
   | "loading-humanize"
   | "show-detect"
   | "show-humanize";
+
+export interface ApiAnalysisResult {
+  ai_rate: number;
+  chunks: {
+    text: string;
+    ai_generated: boolean;
+  }[];
+}
+
+export interface ApiHumanizerResult {
+  previous_rate: number;
+  new_rate: number;
+  humanized_text: string;
+}
+
+export interface InternalAnalysisResult {
+  score: number;
+}
+
+export interface InternalHumanizerResult {
+  originalScore: number;
+  humanizedScore: number;
+  humanizedText: string;
+}

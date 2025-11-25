7220d949a1c77931484804c6e629db50e484e3b5
Ghadeer Akleh <98668965+G-Akleh@users.noreply.github.com>
2025-11-25 09:02:55 +0300

    Modify UI and responsiveness (#25)
    * Modify text box and upload button styles
    
    * Improve responsiveness

--- frontend/src/components/AnalyzeResult.tsx
+++ frontend/src/components/AnalyzeResult.tsx
@@ -19,7 +19,7 @@ const AnalyzeResult: React.FC<AnalyzeResultProps> = ({
 }) => {
   return (
     <div>
-      <label className="text-sm font-medium text-gray-700">Your Text</label>
+      <label className="text-base font-medium text-gray-700">Your Text</label>
       {/* highlighting chunks */}
       <div className="mt-2 w-full h-64 p-4 border border-gray-200 rounded-md bg-white overflow-y-auto whitespace-pre-wrap leading-relaxed cursor-default">
         {result.chunks && result.chunks.length > 0 ? (
@@ -48,23 +48,23 @@ const AnalyzeResult: React.FC<AnalyzeResultProps> = ({
 
       <div className="text-center mt-8">
         <div className="text-sm text-gray-600 mb-2">AI Detection Result</div>
-        <div className="text-7xl font-bold text-red-500">{result.score}%</div>
-        <div className="text-lg text-gray-700 font-medium mt-1">
+        <div className="text-5xl sm:text-7xl font-bold text-red-500">{result.score}%</div>
+        <div className="text-base sm:text-lg text-gray-700 font-medium mt-1">
           Probability of AI
         </div>
       </div>
 
-      <div className="flex justify-center mt-8 gap-4">
+      <div className="flex flex-col sm:flex-row gap-4 justify-center mt-4">
         <button
           onClick={onAnalyzeNew}
-          className="bg-green-500 hover:bg-green-600 text-white font-medium py-3 px-6 rounded-lg flex items-center gap-2 transition-all shadow-md hover:shadow-lg"
+          className="bg-green-500 hover:bg-green-600 text-white font-medium py-3 px-6 rounded-lg flex justify-center items-center gap-2 transition-all shadow-md hover:shadow-lg"
         >
           <ArrowPathIcon className="w-5 h-5" />
           Analyze New Text
         </button>
         <button
           onClick={onHumanizeExisting}
-          className="bg-purple-600 hover:bg-purple-700 text-white font-medium py-3 px-6 rounded-lg flex items-center gap-2 transition-all shadow-md hover:shadow-lg"
+          className="bg-purple-600 hover:bg-purple-700 text-white font-medium py-3 px-6 rounded-lg flex justify-center items-center gap-2 transition-all shadow-md hover:shadow-lg"
         >
           <SparklesIcon className="w-5 h-5" />
           Humanize Text
--- frontend/src/components/TextProcessor.tsx
+++ frontend/src/components/TextProcessor.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { MagnifyingGlassIcon, SparklesIcon, InformationCircleIcon } from "@heroicons/react/24/outline";
+import {
+  MagnifyingGlassIcon,
+  SparklesIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/24/outline";
 import type {
   AppStatus,
   InternalAnalysisResult,
@@ -117,13 +121,18 @@ const TextProcessor: React.FC<TextProcessorProps> = ({
 
   const renderIdleState = () => (
     <div>
-      <div className="flex gap-4 items-start justify-center">
+      <div className="flex gap-4 items-center justify-between">
         <label
           htmlFor="text-input"
           className="text-lg font-medium text-gray-700 py-2"
         >
-          Type Your Text or
+          Type your text
         </label>
+        <div className="hidden md:flex items-center gap-4 w-full max-w-sm">
+          <span className="flex-1 border-t border-gray-300"></span>
+          <span className="text-gray-500 text-sm font-medium">OR</span>
+          <span className="flex-1 border-t border-gray-300"></span>
+        </div>
         <FileUpload
           onTextExtracted={handleFileExtracted}
           onFileNameChange={setUploadedFileName}
@@ -135,11 +144,12 @@ const TextProcessor: React.FC<TextProcessorProps> = ({
         <textarea
           id="text-input"
           className={`
-          mt-2 w-full h-64 p-4 border rounded-md transition focus:outline-none bg-gray-50
-          ${isOverLimit
+          mt-2 w-full h-72 p-4 border rounded-md transition focus:outline-none bg-gray-50
+          ${
+            isOverLimit
               ? "border-red-500 focus:ring-2 focus:ring-red-300"
               : "border-gray-200 focus:ring-2 focus:ring-purple-400 focus:border-transparent"
-            }
+          }
           ${error ? "border-red-500" : ""}
         `}
           placeholder={`Enter text here (up to ${MAX_WORDS} words)...`}
@@ -151,25 +161,22 @@ const TextProcessor: React.FC<TextProcessorProps> = ({
 
       <div className="flex flex-row justify-between mt-2 items-center gap-2">
         {uploadedFileName && (
-
           <div className="min-w-0 flex flex-row items-center gap-2 bg-green-100 text-green-700 text-xs font-semibold py-1.5 px-3 rounded-lg">
             <InformationCircleIcon className="min-w-4 w-4 h-4" />
             <span className="truncate" title={uploadedFileName}>
               {uploadedFileName}
             </span>
           </div>
-
         )}
         <div className="grow" />
         <div
-          className={`shrink-0 text-sm ${isOverLimit ? "text-red-600" : "text-gray-500"}`}
+          className={`shrink-0 text-sm ${
+            isOverLimit ? "text-red-600" : "text-gray-500"
+          }`}
         >
           {wordCount}/{MAX_WORDS} words
         </div>
       </div>
-
-
-
 
       {/* Display API Error Message */}
       {error && (
